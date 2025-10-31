07cd136d5cad1631ba194c3eb728c0ab353ea8b0
mohamedmendou2 <mmendou@iesjulioantonio.cat>
2025-10-31 12:05:31 +0100

    Update RA2_2.2_1_MohamedMendou.py

--- RA2/RA2_2.2_1_MohamedMendou.py
+++ RA2/RA2_2.2_1_MohamedMendou.py
@@ -5,13 +5,12 @@
 # Descripció: Programa que mostra a la pantalla una compta enrere del 10 al 1 i després escriu "Feliç any nou"
 
 # Importa la funció sleep del mòdul time
-from time import sleep 
 
-# Comença la compta enrere des de 10 fins a 1
-for i in [10000, 1, -1]:
-    # Fa una pausa d'1 segon entre cada número
-    sleep(1)
+from time import sleep
+# Inicia la compta enrere des del 10 fins al 1
+for i in range(10, 0, -1):
     print(i)
+    sleep(1)  # Espera 1 segon entre cada número
 
 # Després de la compta enrere, imprimeix "Feliç any nou"
 print("Feliç any nou!")
